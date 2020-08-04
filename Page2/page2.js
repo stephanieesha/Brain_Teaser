3f7581bbda553cafa4c6bbc30279e9c257e0826e
stephanie <stephanieeha@gmail.com>
2020-08-04 23:13:54 +0100

    more functionalities

--- Page2/page2.js
+++ Page2/page2.js
@@ -3,66 +3,32 @@
 function page_load(){
     flip_time();
     swap_time_out();
-    stop_time();
+   
     test();
-}
-
-
-//flip cards to the back
-var flips = true ;
-if(flips == true){
-
-    function flip_time(){
-
-        setTimeout(function(){
-            let my_elements = document.querySelectorAll('.card-flip .card-back');
     
-            let new_elements = my_elements.forEach( x => x.style.transform = 'rotateY(0deg)');
-            
-            return new_elements;
-         }
-         
-        , 15000);
-    }
+    
 }
 
 
+//flip cards to the back after 10 seconds
+function flip_time(){
 
-//flip cards to the front
+    setTimeout(function(){
+        let my_elements = document.querySelectorAll('.card-flip ');
 
-    // document.getElementsByClassName('card-flip').addEventListener('click', () => {
-        // let myElements = document.querySelectorAll('.card-flip .card-back');
-
-        // let newElements = myElements.forEach( x => x.style.transform = 'rotateY(0deg)');
+        let new_elements = my_elements.forEach( x => x.classList.add('flipped'));
+            
+        }
         
-        // return newElements;
+    , 10000);
+}
 
-    //   let  list = document.getElementsByClassName("card-flip");
 
-    //     let new_list = list.forEach(x = x.addEventListener("click", function () {
-    //         alert('hi');
-    //         return new_list;
-    //     }));
-
-        // document.querySelectorAll('.card-flip').forEach(item => {
-        //     item.addEventListener('click', event => {
-        //       //handle click
-
-        //       flips = false;
-         
-        // alert('me');
+//flip card on click
+document.querySelectorAll('.card-flip').forEach(item => {
+item.addEventListener('click', event => item.classList.toggle('flipped')
+    )});
         
-        
-
-        //     })
-        //   })
-        // for (var i = 0; i < list.length; i++) {
-        //   list[i].addEventListener("click", function () {
-        //       alert('hi');
-        //   });
-        // }
-
-       
 
 
 //get card data-value
@@ -112,3 +78,133 @@ function swap_time_out(){
         }, 6000)
           
 }
+
+//correct cards
+// var correct = document.querySelectorAll('.card-flip');
+
+// document.querySelectorAll('.card-flip').forEach(item => {
+//     item.addEventListener('click', event => {
+//         if(divs.getAttribute)
+
+//     }
+//         )});
+var oneClick = 0;
+function  setupCookie(){
+    var inputs3 = document.querySelector('.card-flip');
+
+    
+        oneClick++;
+        if(oneClick ==1 ){
+            return a();
+        }
+    
+        else if(oneClick ==2){
+            return b();
+        }
+
+        if (a() == b()){
+            alert('yipp');
+        }
+        else{
+            alert('up');
+        }
+    
+
+    
+}
+
+// //click card to select
+var count = 0
+var a = function select_card1(){
+//     // var card1 = document.querySelectorAll('.card-flip');
+//     // var card2 = document.querySelectorAll('.card-flip');
+//     // var y = card1.isSameNode(card2); 
+
+    var inputs = document.querySelector('.card-flip');
+   var me =  inputs.getAttribute('value');
+
+
+count++;
+console.log(count);
+if(count == 1){
+
+    console.log('card1: ' + me);
+    return me;
+;}
+}
+// var card_1 = select_card1();
+// console.log(card_1);
+
+var count1 = 0;
+
+
+var b = function select_card2(){
+//     // var card1 = document.querySelectorAll('.card-flip');
+//     // var card2 = document.querySelectorAll('.card-flip');
+//     // var y = card1.isSameNode(card2); 
+
+    var inputs = document.querySelector('.card-flip');
+   var us =  inputs.getAttribute('value');
+
+
+count1++;
+console.log(count1);
+if(count1 == 1){
+
+    
+    console.log('card2: ' + us);
+    return us;
+;}
+}
+
+// var card_2 = select_card2();
+
+// console.log(card_2);
+
+
+
+//    console.log(me1);
+//    //console.log(us);
+//     //console.log(Arr);
+
+//     // console.log(card1);
+//     // console.log(card2);
+
+//     // console.log(y);
+
+
+// function setupCookie() {
+//     document.cookie = document.cookie="click=1; expires=.......";
+// }
+function sss(){
+    if (a == b){
+        alert('yipp');
+    }
+    else{
+        alert('up');
+    }
+}
+
+
+function card_select(){
+    
+    //select_card2();
+    setupCookie();
+    sss();
+}
+
+
+
+
+// create a one-time event
+document.querySelector('.card-flip').addEventListener("click", function us() {
+    console.log('u');
+});
+
+// handler function
+// function handler(e) {
+// 	// remove this handler
+// 	e.target.removeEventListener(e.type, arguments.callee);
+
+// 	alert("You'll only see this once!");
+// }
